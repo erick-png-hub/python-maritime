53fc53d032d674b3a7e0de44d737f91a4c4eb672
Erick-png-hub <erickmig911@gmail.com>
2026-03-11 22:44:49 +0300

    Chapter 2 complete - Flow Control

---
+++ rick.py
@@ -0,0 +1,5 @@
+name = ''
+while name != 'Rick':
+    print('Please type your name.')
+    name = input()
+print('Thank you, Rick!')
---
+++ rick2.py
@@ -0,0 +1,6 @@
+while True:
+    print('Please type your name.')
+    name = input()
+    if name == 'Rick':
+        break
+print('Thank you!')
