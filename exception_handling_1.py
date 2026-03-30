d047af34342239a66db150874eb23f0cd85edf94
Erick-png-hub <erickmig911@gmail.com>
2026-03-30 21:37:54 +0300

    Chapter 3 Continuation

--- exception_handling_1.py
+++ exception_handling_1.py
@@ -2,7 +2,7 @@ def divide(a, b):
     try:
         return a / b
     except ZeroDivisionError:
-        print('Error: Cannot divide by zero')
+        print('Error! Cannot divide by zero')
 
 print(divide(10, 2))
 print(divide(10, 0))
